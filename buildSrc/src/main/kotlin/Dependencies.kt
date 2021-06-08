6d42dbf55ca02a6441a4a90cae7b51afbb1b9dba
y.bond <y.bondarenko@napoleonit.ru>
2021-06-08 16:09:37 +0500

    migrate Groovy to Kotlin

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,11 @@
+object Dependencies {
+
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+    const val junit = "junit:junit:${Versions.junit}"
+
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,28 @@
+object Versions {
+    const val gradle = "4.1.3"
+    const val kotlin = "1.5.10"
+    const val coroutines = "1.5.0"
+
+    const val coreKtx = "1.5.0"
+    const val appcompat = "1.3.0"
+    const val constraintLayout = "2.0.4"
+    const val recyclerView = "1.2.1"
+    const val material = "1.3.0"
+
+    const val junit = "4.13.2"
+
+    const val dagger = "2.36"
+    const val room = "2.3.0"
+    const val moxy = "2.2.2"
+    const val shimmer = "0.5.0"
+    const val skeleton = "1.1.2"
+    const val shimmerLayout = "2.1.0"
+    const val jsoupVersion = "1.13.1"
+    const val viewBindingDelegate = "1.4.6"
+    const val cicerone = "6.2"
+    const val glide = "4.11.0"
+    const val timber = "4.7.1"
+    const val kotpref = "2.12.0"
+}
+
+
